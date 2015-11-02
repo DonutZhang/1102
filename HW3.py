cda3a9c3e87f376e59d18bcf332902d3b9f826a9
DonutZhang <b10231034@mail.ntust.edu.tw>
2015-11-02 15:11:41 +0800

    test

---
+++ HW3.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[1]:
+
+years_list = [1980, 1981, 1982, 1983, 1984, 1985]
+years_list
+
+
+# In[2]:
+
+years_list[2]
+
+
+# In[3]:
+
+sorted_years = sorted(years_list)
+sorted_years[-1]
+
+
+# In[ ]:
+
+
+
